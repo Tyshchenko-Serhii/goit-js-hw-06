a88083a75c23b011c68a6055797c2c28e9407cc6
Tars-El <106482481+Tars-El@users.noreply.github.com>
2022-11-06 21:51:37 +0200

    Summary

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,12 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const email = event.currentTarget.elements.email.value;
+  const password = event.currentTarget.elements.password.value;
+
+  email === "" || password === ""
+    ? alert("Все поля должны быть заполнены!")
+    : console.log({ email, password });
+});
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector("body");
+const changeBtn = document.querySelector(".change-color");
+const colorName = document.querySelector(".color");
+
+const changeColor = () => {
+  body.style.backgroundColor = getRandomHexColor();
+  colorName.textContent = getRandomHexColor();
+};
+
+changeBtn.addEventListener("click", changeColor);
